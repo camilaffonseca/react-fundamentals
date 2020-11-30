ce84d0cc6c5ab4cc49d3b1b809482b3975a65bb1
Camila Fonseca <camilafonsecafp@outlook.com>
2020-11-30 10:07:28 -0300

    Finished Card component

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -2,17 +2,29 @@ import React from 'react'
 
 import './Card.css'
 
-const Card = ({title, children}) => {
-    return (
-        <div className='Card'>
-            <div className='Title'>{title}</div>
-            <div className='Content'>{children}</div>
-        </div>
-    )
+const Card = ({ title, children, backgroundColor, contentBackgroundColor, textColor }) => {
+  const cardStyle = {
+    backgroundColor: backgroundColor || 'rgb(7, 31, 51)',
+    borderColor: backgroundColor || 'rgb(7, 31, 51)',
+  }
+
+  const cardContentStyle = {
+    backgroundColor: contentBackgroundColor || 'rgb(36, 78, 112)',
+    color: textColor || 'white',
+  }
+
+  return (
+    <div className='Card' style={cardStyle}>
+      <div className='Title'>{title}</div>
+      <div className='Content' style={cardContentStyle}>
+        {children}
+      </div>
+    </div>
+  )
 }
 
 Card.defaultProps = {
-    title: 'Title undefined'
+  title: 'Title undefined',
 }
 
 export default Card
